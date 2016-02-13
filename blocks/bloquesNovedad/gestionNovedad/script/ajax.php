2d971a54be4ff34cbdf467410db18ba499687e14
blacburn <andreysa0114@hotmail.com>
2016-02-13 13:49:38 -0500

    Formulario campos en resgistro Esporádico Tipo Campo para formularios

--- blocks/bloquesNovedad/gestionNovedad/script/ajax.php
+++ blocks/bloquesNovedad/gestionNovedad/script/ajax.php
@@ -24,7 +24,7 @@ $(document).ready(function() {
 	                 
 			// Añadir elementos Dinamicos en el DOM
 			
-			$(container).append('<fieldset id=panel'+iCnt+' class="ui-widget ui-widget-content">'+
+			$(container).append('<fieldset id=panel '+iCnt+' class="ui-widget ui-widget-content">'+
 					'<legend class="ui-state-default ui-corner-all"> CAMPO'+iCnt+'</legend>'+
 					'<div id=lab1'+iCnt+' class="col-md-2">'+
 						'<label> Nombre del Campo:  </label> ' + 
@@ -45,8 +45,8 @@ $(document).ready(function() {
 					'<select id=tipoDato'+iCnt+'><option value="Alfanumerico">Alfanumérico</option>'+
                                         '<option value="Valor">Valor</option>'+
                                         '<option value="Lista">Lista</option>'+
-                                        '<option value="Fecha">Audi</option>'+
-                                        '<option value="Tabla">Audi</option>'+
+                                        '<option value="Fecha">Fecha</option>'+
+                                        '<option value="Tabla">Tabla</option>'+
                                         '</select>'+
                                         '</div>'+
                                         '<br/>'+
@@ -54,19 +54,27 @@ $(document).ready(function() {
 						'<div id=lab2'+iCnt+' class="col-md-2">'+
 							'<label> Requerido: </label> ' + 
 						'</div>'+
-					'<input type=text class="input" id=tb2' + iCnt + ' size="80"  maxlength="500" value="" onBlur="devPos2('+iCnt+')"/>'+
+					'<select id=requerido'+iCnt+'><option value="No">No</option>'+
+                                        '<option value="Si">Si</option>'+
+                                        '</select>'+
                                         '</div>'+
                                         '<br/>'+
 					'<div>'+
 						'<div id=lab2'+iCnt+' class="col-md-2">'+
 							'<label> Fórmula: </label> ' + 
 						'</div>'+
-					'<input type=text class="input" id=tb2' + iCnt + ' size="80"  maxlength="500" value="" onBlur="devPos2('+iCnt+')"/>'+
+					'<select id=formulacionCampo'+iCnt+'><option value="No">No</option>'+
+                                        '<option value="Si">Si</option>'+
+                                        '</select>'+
                                         '</div>'+ 
 					'</fieldset>');
 			$('#camposDinamicos').after(container);
 			$('#tipoDato'+iCnt).width(250);
                         $('#tipoDato'+iCnt).select2();
+                        $('#requerido'+iCnt).width(250);
+                        $('#requerido'+iCnt).select2();
+                        $('#formulacionCampo'+iCnt).width(250);
+                        $('#formulacionCampo'+iCnt).select2(); 
                  
 	});
 	
